8e325ef2213f49af4168ecf4f72b235a67a663f7
GameChanger77 <gamer4christ2004@gmail.com>
2023-10-15 15:51:49 -0500

    Python test script

---
+++ script.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#
+#
+# sudo apt-get install git-core
+
+# git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+
+# sudo apt-get install build-essential python-dev
+
+# sudo python setup.py install
+#
+#
+import sys
+import Adafruit_DHT
+
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+    print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
